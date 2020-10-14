a553041d818a7b0ef2b1389235666f3f118cbe8d
Uriel Reyes <uriel1012@yahoo.com>
2020-10-14 12:50:20 -0500

    update comments on handler.js

--- TwilioManager/handler.js
+++ TwilioManager/handler.js
@@ -28,13 +28,11 @@ module.exports.twilioManager = async event => {
   let data = JSON.parse(event.body);
   console.log("getOrderDataProducts", data);
 
-  //get order order data
+//get order order data
     const orderData = await getOrderData(data.data.orderId);
     console.log("twilio", `https://bigcommerce-9819.twil.io/order_sms?number=${orderData.billing_address.phone}`)
 
-    //then send phone numbner to twiliio like https://bigcommerce-9819.twil.io/order_ms?number=+315-224-6874
-    //get phone number into right format
-// https://bigcommerce-9819.twil.io/order_sms?number=315-224-6874
+//send to twilio like this https://bigcommerce-9819.twil.io/order_sms?number=315-224-6874
 const options = {
   method: "GET",
   uri: `https://bigcommerce-9819.twil.io/order_sms?number=+1${orderData.billing_address.phone}`
